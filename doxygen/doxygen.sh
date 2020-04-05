46ba409b7151d499b5aaba8cc08b04d4f16f3b0e
Jin Huang <blackcathj@gmail.com>
2020-04-05 18:28:11 -0400

    Update doxygen.sh

--- doxygen/doxygen.sh
+++ doxygen/doxygen.sh
@@ -14,8 +14,8 @@ echo "dot at " `which dot`
 
 
 echo "-------- Doxyfile additional setup ---------"
-echo "CVS_STRIP_FROM_PATH = " `pwd` | tee -a Doxyfile
-echo "INPUT                  = " `/bin/ls -d */` | tee -a Doxyfile
+echo "CVS_STRIP_FROM_PATH = " `pwd`/ | tee -a Doxyfile
+echo "INPUT               = " doxygen_mainpage.h `/bin/ls -d */` | tee -a Doxyfile
 
 echo "-------- start doxygen ---------"
 
